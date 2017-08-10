7b3b1b86c11c72b5e6700a9a8fb7b88a13adbfd1
alexr101 <alexonezero@outlook.com>
2017-08-10 10:47:46 -0400

    application restructure, in preparation for Gulp

--- README.md
+++
@@ -1,8 +0,0 @@
-# avid-reader
-
-Built with Angular, JS, HTML, &amp; SASS
-
-Start a local server to run project.  
-
-python -m SimpleHTTPServer 
-
--- components/about/AboutCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('app')
-
-  .controller('AboutCtrl', ['$scope', '$http', 
-    function($scope, $http){
-      
-      $scope.aboutUs = "About us";
-      $scope.content = "we are a unique company that creates custom bookshelves for the avid book lover!"
-    }]);
